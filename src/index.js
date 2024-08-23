4bfb63dd63d92f3ef96aba11f711e9c6612daf65
zkc <xco114514myasus@tom.com>
2024-08-23 11:45:11 +0800

    Normal Development 2
    README Update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom/client';
 // 导入项目根组件
 import App from './App';
 
+// 把 App 根组件渲染到 id 为 root 的 DOM 节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
